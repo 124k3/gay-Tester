2d2311b568bf1d58ff98cffc49730494c514a495
1243k <akha3743@gmail.com>
2024-09-17 06:12:17 -0700

    comments for takeStringInput, printConormDialog methods

--- src/io/github/124k3/GameUI.java
+++ src/io/github/124k3/GameUI.java
@@ -88,11 +88,11 @@ public class GameUI {
     }
 
     /**
-     *
-     *
-     *
-     *
-     *
+     * presents an input box to obtain data (in the form ) of string from the user.
+     * @param frame the parent window created via JFrame
+     * @param question the string message to be shown to the user.
+     * @param optionType 1,2,3,4 changes the icon of the prompt.
+     * @returns the string obtained from the user.
      */
     public String takeStringInput(
         JFrame frame,
@@ -107,7 +107,13 @@ public class GameUI {
         );
     }
 
-    /***/
+    /**
+     * provided a conformation dialog box with yes_no, yes_cancel, yes_no_cancel
+     * buttons and a message for the user.
+     * @param frame the JFrame object.
+     * @param message the message string which the user sees.
+     * @param optionType integer value which allows the selection of buttons.
+     **/
     public void printConformDialog(
         JFrame frame,
         String message,
